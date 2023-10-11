77d4cab9a97708a57ac2524f4e2d6ee94629cb37
VfanLee <fanfanfafafa@gmail.com>
2023-10-11 18:07:13 +0800

    i18n: Message Format Syntax

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,18 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const calendarValue = ref(new Date())
+const date = computed(() => {
+  const d = new Date()
+  return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`
+})
 </script>
 
 <template>
   <div class="home">
     <h2>{{ $t('home') }}</h2>
+
+    <section>{{ $t('todayis', { date: date }) }}</section>
 
     <section>
       <el-calendar v-model="calendarValue" />
