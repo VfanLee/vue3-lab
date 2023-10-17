0834b76ae32e5afcc05f4a7ab0835c777b00d7ec
VfanLee <fanfanfafafa@gmail.com>
2023-10-17 14:47:33 +0800

    reactivity

--- src/views/basic/reactivity.vue
+++ src/views/basic/reactivity.vue
@@ -7,8 +7,21 @@ const arr = ref([])
 const updateObj = () => {
   obj.value = { id: '001:update', name: 'foo:update', gender: 'male:update' }
 }
+const addObjFoo = () => {
+  obj.value.foo = Date.now()
+}
+const removeObjFoo = () => {
+  delete obj.value.foo
+}
+
 const updateArr = () => {
   arr.value = [4, 5, 6]
+}
+const updateArrIndexValue = () => {
+  arr.value[1] = Date.now()
+}
+const updateArrLength = () => {
+  arr.value.length = 1
 }
 
 obj.value = { id: '001', name: 'foo', gender: 'male' }
@@ -19,11 +32,16 @@ arr.value = [1, 2, 3]
   <h2>Reactivity</h2>
 
   <section>
-    obj: {{ obj }}
+    <p>obj: {{ obj }}</p>
     <button type="button" @click="updateObj">update obj</button>
+    <button type="button" @click="addObjFoo">add obj.foo</button>
+    <button type="button" @click="removeObjFoo">remove obj.foo</button>
   </section>
+
   <section>
-    arr: {{ arr }}
+    <p>arr: {{ arr }}</p>
     <button type="button" @click="updateArr">update arr</button>
+    <button type="button" @click="updateArrIndexValue">update arr[1]</button>
+    <button type="button" @click="updateArrLength">update arr length</button>
   </section>
 </template>
