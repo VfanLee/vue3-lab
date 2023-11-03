603b69f0aac208e115cc12e8aae225545af0d00d
VfanLee <fanfanfafafa@gmail.com>
2023-11-03 14:12:28 +0800

    theme

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ based on [create-vue](https://github.com/vuejs/create-vue).
 ## 自定义主题踩坑记录
 
 1. Element Plus 全局引入：注意在引入自定义 css 变量文件之前引入 Element Plus 样式。
-   1. Tip：该方式可实现自定义主题，但加载资源过多，生产环境不建议。
-2. Element Plus 自动按需引入：由于按需加载，样式延迟加载，会导致自定义 css 变量被覆盖。
-   1. ❌ 暂未想到合适的解决方案。
-3. ✔ Element Plus 手动按需引入：由于组件内按需引入的话会导致自定义 css 变量被覆盖，所以一定要全局注册 Element Plus 组件，且需要在自定义 css 变量文件之前引入。
+   1. 加载资源过多，生产环境不建议。
+2. Element Plus 手动按需引入：由于组件内按需引入的话会导致自定义 css 变量被覆盖，所以一定要全局注册 Element Plus 组件，且需要在自定义 css 变量文件之前引入。
+   1. 每次使用组件时，都需要手动引入，比较麻烦。
+3. Element Plus 自动按需引入：由于按需加载，样式延迟加载，会导致自定义 css 变量被覆盖，所以要提高 Element Plus 自定义 css 变量的优先级。
--- src/main.js
+++ src/main.js
@@ -3,20 +3,20 @@ import store from './store'
 import router from './router'
 
 // 注意：一定要在自定义 css 变量之前引入组件，不然会导致自定义属性被覆盖
-import { ElButton, ElInput, ElDatePicker, ElDrawer, ElForm, ElFormItem, ElSelect, ElOption, ElDivider, ElDropdown, ElDropdownMenu, ElDropdownItem } from 'element-plus'
+// import { ElButton, ElInput, ElDatePicker, ElDrawer, ElForm, ElFormItem, ElSelect, ElOption, ElDivider, ElDropdown, ElDropdownMenu, ElDropdownItem } from 'element-plus'
 import App from './App.vue'
 
 import 'normalize.css'
 import '@/styles/index.scss'
-import '@/styles/theme/index.css'
+import '@/styles/theme/index.scss'
 
 const app = createApp(App)
 
 // 全局注册 Element Plus 组件
-const elementPlusComponents = { ElButton, ElInput, ElDatePicker, ElDrawer, ElForm, ElFormItem, ElSelect, ElOption, ElDivider, ElDropdown, ElDropdownMenu, ElDropdownItem }
-for (const component in elementPlusComponents) {
-  app.component(elementPlusComponents[component].name, elementPlusComponents[component])
-}
+// const elementPlusComponents = { ElButton, ElInput, ElDatePicker, ElDrawer, ElForm, ElFormItem, ElSelect, ElOption, ElDivider, ElDropdown, ElDropdownMenu, ElDropdownItem }
+// for (const component in elementPlusComponents) {
+//   app.component(elementPlusComponents[component].name, elementPlusComponents[component])
+// }
 
 app
   .use(store)
